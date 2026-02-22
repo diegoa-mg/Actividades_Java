d22e525e8e0f933feedf704295162a90a643371c
diegoa-mg <ddiegoa.mg@gmail.com>
2026-02-22 14:45:15 -0600

    Actividad 8

--- Act8/src/CuentaBancaria.java
+++ Act8/src/CuentaBancaria.java
@@ -1,2 +1,22 @@
 public class CuentaBancaria {
+    String titular, numeroCuenta;
+    double saldo;
+
+    public CuentaBancaria(String titular, String numeroCuenta, double saldo){
+        this.titular = titular;
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+    }
+
+    public double depositar(double cantidad){
+        return saldo += cantidad;
+    }
+
+    public double retirar(double cantidad){
+        return saldo -= cantidad;
+    }
+
+    public void mostrarSaldo(){
+        System.out.println("Saldo: " + saldo + " pesos.");
+    }
 }
--- Act8/src/Main.java
+++ Act8/src/Main.java
@@ -1,8 +1,50 @@
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner();
+        Scanner sc = new Scanner(System.in);
+        double cantidad = 0;
+        char op;
 
+        System.out.print("Ingresa el titular de la cuenta: ");
+        String titu = sc.nextLine();
+
+        System.out.print("Ingresa el numero de la cuenta: ");
+        String num = sc.nextLine();
+
+        System.out.print("Ingresa el saldo de la cuenta: ");
+        int saldo = sc.nextInt();
+        sc.nextLine();
+
+        CuentaBancaria MiCuenta = new CuentaBancaria(titu, num, saldo);
+
+        System.out.print("Desea realizar un deposito? (s/n): ");
+        op = sc.next().charAt(0);
+
+        if (op == 's' || op == 'S'){
+            System.out.print("Ingrese la cantidad: ");
+            cantidad = sc.nextDouble();
+            MiCuenta.depositar(cantidad);
+        }
+
+        System.out.print("Desea realizar un retiro? (s/n): ");
+        op = sc.next().charAt(0);
+
+        if (op == 's' || op == 'S'){
+            System.out.print("Ingrese la cantidad: ");
+            cantidad = sc.nextDouble();
+            if (cantidad <= saldo){
+                MiCuenta.retirar(cantidad);
+            }
+            else {
+                System.out.println("Error: No puede retirar un valor mayor a " + saldo + " pesos.");
+            }
+        }
+
+        MiCuenta.mostrarSaldo();
+
+        sc.close();
     }
 }
