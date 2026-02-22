90a7ca81670784a22148ee7396d7090976e1cf9b
diegoa-mg <ddiegoa.mg@gmail.com>
2026-02-22 13:29:35 -0600

    Actividad 7

--- Act7/src/Libro.java
+++ Act7/src/Libro.java
@@ -1,3 +1,14 @@
 public class Libro {
+    String titulo, autor;
+    int anioPublicacion;
 
+    public Libro(String titulo, String autor, int anioPublicacion){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anioPublicacion = anioPublicacion;
+    }
+
+    public void resumen(){
+        System.out.println("Título: " + titulo + ", Autor: " + autor + ", Año de publicación " + anioPublicacion);
+    }
 }
--- Act7/src/Main.java
+++ Act7/src/Main.java
@@ -1,15 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Ingresa el titulo del libro: ");
+        String titu = sc.nextLine();
+
+        System.out.println("Ingresa el autor del libro: ");
+        String autor = sc.nextLine();
+
+        System.out.println("Ingresa el año de publicación del libro: ");
+        int anio = sc.nextInt();
+
+        Libro MiLibro = new Libro(titu, autor, anio);
+
+        MiLibro.resumen();
+
+        sc.close();
     }
 }
