4035e103e5e8ffec3595278cf40110ee2e2f31f1
diegoa-mg <ddiegoa.mg@gmail.com>
2026-02-24 12:40:19 -0600

    Avance actividad 11

---
+++ Act11/src/Estudiante.java
@@ -0,0 +1,14 @@
+public class Estudiante {
+    String nombre, matricula;
+    double[] calificaciones;
+
+    public Estudiante(String nombre, String matricula, double[] calificaciones){
+        this.nombre = nombre;
+        this.matricula = matricula;
+        this.calificaciones = calificaciones;
+    }
+
+    public void calcularPromedio(){
+        for (int i = 0; i <)
+    }
+}
