900f542af8d2cb8287b754f4fb973a48c4a212e2
Diego Alejandro ⚽ <ddiegoa.mg@gmail.com>
2026-04-13 22:03:01 -0600

    Avances Act20

---
+++ Act20/src/1/src/Estudiante.java
@@ -0,0 +1,10 @@
+public class Estudiante extends Persona {
+    String matricula;
+
+    public Estudiante(String nombre, int edad, String matricula){
+        super(nombre, edad);
+        this.matricula = matricula;
+    }
+
+
+}
---
+++ Act20/src/1/src/Persona.java
@@ -0,0 +1,22 @@
+public class Persona {
+    String nombre;
+    int edad;
+
+    public Persona(String nombre, int edad){
+        if (nombre.length() < 3){
+            System.out.println("Error: Nombre muy corto.");
+            return;
+        }
+        if (edad < 6 || edad > 100){
+            System.out.println("Error: Edad invalida (6-100)");
+            return;
+        }
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public void mostrarDatos(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+    }
+}
